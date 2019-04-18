9424e376593bb5139e22bf9e31e53113094be2d4
Mika Viinamäki <mika.viinamaki@fraktio.fi>
2019-04-18 15:28:29 +0300

    Oops floats are difficult

--- src/SyncStartScoreKeeper.cpp
+++ src/SyncStartScoreKeeper.cpp
@@ -16,7 +16,7 @@ float getScore(const SyncStartScore& score) {
 		return 0;
 	}
 
-	return clamp(score.data.actualDancePoints / score.data.possibleDancePoints, 0, 1);
+	return clamp(score.data.actualDancePoints / (float) score.data.possibleDancePoints, 0, 1);
 }
 
 int getLostDancePoints(const SyncStartScore& score) {
